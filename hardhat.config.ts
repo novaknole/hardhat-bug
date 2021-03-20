95dab8ccb9f2bacc57329ba257904640eec2fb00
Giorgi-Lagidze <giorgi.lagidze@toptal.com>
2021-03-21 01:12:46 +0400

    first

---
+++ hardhat.config.ts
@@ -0,0 +1,38 @@
+import { config as dotenvConfig } from 'dotenv'
+import { resolve } from 'path'
+dotenvConfig({ path: resolve(__dirname, './.env') })
+
+import { HardhatUserConfig } from 'hardhat/types'
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-etherscan'
+import '@nomiclabs/hardhat-waffle'
+import 'hardhat-deploy'
+import 'hardhat-typechain'
+import 'solidity-coverage'
+
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.6.8',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 20000, // TODO: target average DAO use
+      },
+    },
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+  etherscan: {
+    apiKey: "YNEE235H7HTDQ5V5T1WAIDFRMJGIJND925"
+  },
+  networks: {
+    rinkeby: {
+      url: 'https://rinkeby.eth.aragon.network',
+      accounts: [`0x188f20fbb60eaf10ca87088ace8d4c20bb5687848ee462044db4a9ad442dcc81`]
+    },
+  },
+}
+
+export default config
